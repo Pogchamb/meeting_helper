56747a1bb6c36c433ee828540275fc83c82c283b
Pa_chan <poghcamb1@gmail.com>
2026-07-01 01:32:51 +0700

    add AudioRecorderService.kt and override on Create

--- core/audio/src/main/java/pa/chan/audio/AudioRecorderService.kt
+++ core/audio/src/main/java/pa/chan/audio/AudioRecorderService.kt
@@ -1,8 +1,12 @@
 package pa.chan.audio
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 
 class AudioRecorderService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
@@ -12,6 +16,23 @@ class AudioRecorderService : Service() {
 
     override fun onCreate() {
         super.onCreate()
+        val importance = NotificationManager.IMPORTANCE_LOW
+        val channelId = "AudioRecordNotifyID"
+        val channelName = "AudioRecordNotify"
+
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationChannel = NotificationChannel(channelId, channelName, importance)
+        notificationChannel.description = "notify audio record"
+        notificationChannel.setShowBadge(false)
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(android.R.drawable.ic_btn_speak_now)
+            .build()
+
+        startForeground(1, notification)
     }
 
     override fun onDestroy() {
